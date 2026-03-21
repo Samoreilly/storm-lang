af0e6907b1beccb75936b3d51af543562cae823e
Samoreilly <x23756575@student.ncirl.ie>
2026-03-21 18:00:19 +0000

    refactor

---
+++ storm/compiler/parser/parser_expression.cpp
@@ -0,0 +1,185 @@
+#include "node.h"
+#include "condition.h"
+#include "parser.h"
+#include "../../token.h"
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <regex>
+#include <iostream>
+
+std::unique_ptr<Condition> Parser::parse_proc_call() {
+
+    auto call = std::make_unique<ProcCallNode>();
+
+    call->proc_name = get_token().value;
+    advance();
+    consume(TokenType::SYMBOL, "(");
+
+    while(!check(TokenType::SYMBOL, ")")) {
+        call->arguments.push_back(parse_condition());
+
+        if(check(TokenType::SYMBOL, ",")) {
+            advance();
+        }
+    }
+
+    consume(TokenType::SYMBOL, ")");
+   
+    std::cerr << "Current token: " << get_token().value << "\n";
+    return call;
+}
+
+std::unique_ptr<Condition> Parser::parse_condition() {
+    return parse_comparison();
+}
+
+std::unique_ptr<Condition> Parser::parse_comparison() {
+
+    std::unique_ptr<Condition> left = parse_add();
+
+    while(check(TokenType::OPERATOR, "<") || check(TokenType::OPERATOR, ">") || 
+          check(TokenType::OPERATOR, "<=") || check(TokenType::OPERATOR, ">=") || 
+          check(TokenType::OPERATOR, "==") || check(TokenType::OPERATOR, "!=")) {
+        
+        Token op = get_token();
+        advance();
+
+        auto right = parse_add();
+
+        auto node = std::make_unique<BinaryExpression>();
+        node->left = std::move(left);
+        node->op = op.value;
+        node->right = std::move(right);
+
+        left = std::move(node);
+    }
+
+    return left;
+}
+
+std::unique_ptr<Condition> Parser::parse_add() {
+
+    std::unique_ptr<Condition> left = parse_mul();
+
+    while(check(TokenType::OPERATOR, "+") || check(TokenType::OPERATOR, "-")) {
+        Token op = get_token();
+        advance();
+
+        auto right = parse_mul();
+
+        auto node = std::make_unique<BinaryExpression>();
+
+        node->left = std::move(left);
+        node->op = op.value;
+        node->right = std::move(right);
+
+        left = std::move(node);
+    }
+
+    return left;
+
+}
+
+std::unique_ptr<Condition> Parser::parse_mul() {
+
+    std::unique_ptr<Condition> left = parse_primary();
+
+    while(check(TokenType::OPERATOR, "*") || check(TokenType::OPERATOR, "/")) {
+        Token op = get_token();
+        advance();
+
+        auto right = parse_primary();
+
+        auto node = std::make_unique<BinaryExpression>();
+
+        node->left = std::move(left);
+        node->op = op.value;
+        node->right = std::move(right);
+
+        left = std::move(node);
+    }
+
+    return left;
+}
+
+std::unique_ptr<Condition> Parser::parse_primary() {
+
+    Token curr = get_token();
+
+    std::regex is_alpha("[a-zA-Z]");
+    std::regex is_num("[0-9]");
+
+    switch(curr.type) {
+
+        case TokenType::STRING: {
+            advance();            
+            return std::make_unique<StringCondition>(curr);
+            break;
+        }
+
+        case TokenType::DOUBLE: {
+            advance(); 
+            if(std::regex_search(curr.value, is_alpha)) {
+                throw std::runtime_error("Characters are not allowed in integers"
+                                         + std::to_string(curr.line) + " col: "
+                                         + std::to_string(curr.col) + "\n");
+            
+            }
+
+            return std::make_unique<DoubleCondition>(curr);
+            break;
+        }
+
+        case TokenType::INTEGER: {
+             
+            std::cerr << "Integer token: " << get_token().value << "\n";
+            
+            if(curr.value.find('.') != std::string::npos || std::regex_search(curr.value, is_alpha)) {
+                throw std::runtime_error("\nDecimal points or characters are not allowed in integers line: "
+                                         + std::to_string(curr.line) + " col: "
+                                         + std::to_string(curr.col) + "\n");
+            }
+
+            advance();
+            return std::make_unique<IntegerCondition>(curr);
+
+        }
+
+        case TokenType::BOOL: {
+            advance(); 
+            if(curr.value != "true" && curr.value != "false") {
+                throw std::runtime_error("Boolean values can only be true or false"
+                                         + std::to_string(curr.line) + " col: "
+                                         + std::to_string(curr.col) + "\n");
+            }
+            return std::make_unique<BoolCondition>(curr);
+        }
+
+        case TokenType::CHAR: {
+            advance(); 
+            if(curr.value.length() != 1) {
+                throw std::runtime_error("Char must only be 1 character Line: "
+                                         + std::to_string(curr.line) + " col: "
+                                         + std::to_string(curr.col) + "\n");
+            }
+            
+            return std::make_unique<CharCondition>(curr);
+        }
+        
+        case TokenType::IDENTIFIER: {
+            if (peek_next(1).value == "(") {
+                return parse_proc_call();
+            }
+            advance();
+            return std::make_unique<IdentifierCondition>(curr);
+        }
+
+        //default to string is safest
+        default:
+            advance(); 
+            return std::make_unique<StringCondition>(curr);
+
+    };
+
+}
